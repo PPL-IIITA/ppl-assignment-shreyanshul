3801b33880a811670891dd25de590316d80f4aca
Shreyansh <shreyanshdk@gmail.com>
2017-04-09 23:47:23 +0530

    Add files via upload

---
+++ Ques6/src/gifts.py
@@ -0,0 +1,12 @@
+class gifts:
+	
+	def __init__( self, name, price, value, types) :
+		"Initializing variables through constructor"
+		self.name = name
+		"""@ivar: Gift's Name"""
+		self.price = price
+		"""@ivar: Price of Gift"""
+		self.value = value
+		"""@ivar: Value of Gift"""
+		self.types = types
+		"""@ivar: Type of Gift"""
